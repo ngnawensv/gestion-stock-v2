1a96e74e3f5eb58501e19f4cf9dcd6f82b4c20ae
Ngnawen Samuel <ngnawens@gmail.com>
2021-08-04 10:21:36 +0400

    category service and component implemented

--- stock-client-v2/src/app/pages/categories/nouvelle-categorie/nouvelle-categorie.component.ts
+++ stock-client-v2/src/app/pages/categories/nouvelle-categorie/nouvelle-categorie.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CategorieService } from 'src/app/services/categorie/categorie.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { CategorieDto } from 'src/gs-api/src/models';
 
 @Component({
   selector: 'app-nouvelle-categorie',
@@ -8,9 +11,28 @@ import { Router } from '@angular/router';
 })
 export class NouvelleCategorieComponent implements OnInit {
 
-  constructor(private route:Router) { }
+  categorieDto: CategorieDto = {};
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private categorieService: CategorieService,
+    private userService: UserService,
+    private route: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  saveCategorie(): void{
+    this.categorieDto.entrepriseId = this.userService.getConnectedUser()?.entreprise?.id;
+    this.categorieService.saveCategorie(this.categorieDto).subscribe(
+      data => {
+        this.route.navigate(['categories']);
+       },
+      error => {
+        this.errorMsg = error.error.errors;
+      }
+      );
   }
 
   cancelClick():void{
--- stock-client-v2/src/app/pages/categories/page-categories/page-categories.component.ts
+++ stock-client-v2/src/app/pages/categories/page-categories/page-categories.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CategorieDto } from '../../../../gs-api/src/models/categorie-dto';
+import { CategorieService } from '../../../services/categorie/categorie.service';
 
 @Component({
   selector: 'app-page-categories',
@@ -8,10 +10,31 @@ import { Router } from '@angular/router';
 })
 export class PageCategoriesComponent implements OnInit {
 
-  constructor(private route:Router){ }
+
+  listeCategorie: Array<CategorieDto> = [];
+
+  constructor(
+    private route: Router,
+    private categoriesService: CategorieService
+  ) { }
 
   ngOnInit(): void {
+    this.findAllCategorie();
   }
+
+
+  findAllCategorie() {
+    this.categoriesService.findAll().subscribe(
+      data => {
+        this.listeCategorie = data;
+      },
+      error => {
+
+      }
+    );
+  }
+
+
 
   nouvelleCategorie():void{
     this.route.navigate(['nouvellecategorie']);
